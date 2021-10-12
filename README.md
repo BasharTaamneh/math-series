6a25729bf7e025ff67d1d62909e2261715e8d7f3
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-10-12 06:22:43 +0300

    done testing

--- README.md
+++ README.md
@@ -34,6 +34,6 @@ python -m Modules_and_Testing.math_series
 
 - [x] Both the fibonacci series and the lucas numbers are based on an identical formula. Add a third function called sum_series with one required parameter and two optional parameters. The required parameter will determine which element in the series to print. The two optional parameters will have default values of 0 and 1 and will determine the first two values for the series to be produced.
 
-- [ ] Calling this function with no optional parameters will produce numbers from the fibonacci series. Calling it with the optional arguments 2 and 1 will produce values from the lucas numbers. Other values for the optional parameters will produce other series. Again, you may use recursion or iteration, or both. Again, ensure that your function has a well-formed docstring.
+- [x] Calling this function with no optional parameters will produce numbers from the fibonacci series. Calling it with the optional arguments 2 and 1 will produce values from the lucas numbers. Other values for the optional parameters will produce other series. Again, you may use recursion or iteration, or both. Again, ensure that your function has a well-formed docstring.
   
-- [ ] Add /series.py and test_series.py modules to your repository and commit frequently while working on your implementation. Include good commit messages that explain concisely both what you are doing and why.
+- [x] Add /series.py and test_series.py modules to your repository and commit frequently while working on your implementation. Include good commit messages that explain concisely both what you are doing and why.
--- math_series/series.py
+++ math_series/series.py
@@ -6,11 +6,13 @@
 def fibonacci(n: int) -> int:
     '''
     The Fibonacci Series is a numeric series starting with the integers 0 and 1.
-        In this series, the next integer is determined by summing the previous two integers.
-        This gives us: (n)=6, (nth_value)=[0, 1, 1, 2, 3, 5, 8, 13, ...]
-    the fibonacci function have one parameter (n).
-        so will return the nth value in the fibonacci series after checking that the parameter (n),
-        greater than or equal to zero.
+    In this series, the next integer is determined by summing the previous two integers.
+    The Fibonacci function has one parameter (n).
+    so depends on this parameter the Fibonacci function will iterate the (n)loop,
+    to create the series by summing the previous two integers and,
+    appending the summation to the Fibonacci Series then
+    return the (nth_value) from it.
+    This gives us: (n)=6, (nth_value)=[0, 1, 1, 2, 3, 5, 8, 13, ...] the return nth=(13)
     '''
     fibonacci_sequence = [0, 1]
     for i in range(n):
@@ -23,12 +25,15 @@ def fibonacci(n: int) -> int:
 def lucas(n: int) -> int:
     '''
     The Lucas Numbers are a related series of integers,
-        that start with the values 2 and 1 rather than 0 and 1.
-        In this series, the next integer is determined by summing the previous two integers.
-        This gives us: (n)=6, (nth_value)=[2, 1, 3, 4, 7, 11, 18, 29, ...]
+    that start with the values 2 and 1 rather than 0 and 1.
+    In this series, the next integer is determined by summing the previous two integers.
     the Lucas function have one parameter (n).
-        so will return the nth value in the Lucas series after checking that the parameter (n),
-        greater than or equal to zero.
+    so depends on this parameter the Lucas function will iterate the (n)loop,
+    to create the series by summing the previous two integers and,
+    appending the summation to the Lucas Numbers then
+    return the (nth_value) from it.
+    so will return the nth value in the Lucas series.
+    This gives us: (n)=6, (nth_value)=[2, 1, 3, 4, 7, 11, 18, 29, ...] the return nth=(29)
     '''
     lucas_numbers = [2, 1]
     for i in range(n):
@@ -39,6 +44,22 @@ def lucas(n: int) -> int:
 
 
 def sum_series(n: int, x=0, y=1):
+    '''
+    The sum_series is a function that generates a series
+    starting with defulet two optional arguments integers 0 and 1.
+    In this series, the next integer is determined by summing the previous two integers.
+    The sum_series function has one parameter (n).
+    so depends on this parameter the sum_series function will iterate the (n)loop,
+    to create the series by summing the previous two integers and,
+    appending the summation to the Series then
+    return the (nth_value) from it same as the Fibonacci function
+    but if you want to generate a different series you have to change the optional value,
+    by-->passing the new value through the function call.
+    sum_series(6)=> this gives us: (n)=6,
+    (nth_value)=[0, 1, 1, 2, 3, 5, 8, 13, ...] the return nth=(13)
+    sum_series(6, 2, 1)=> this gives us: (n)=6, (x)=2, (y)=1
+    (nth_value)=[2, 1, 3, 4, 7, 11, 18, 29, ...] the return nth=(29)
+    '''
     series = [x, y]
     for i in range(n):
         nth_value = series[i] + series[i+1]
