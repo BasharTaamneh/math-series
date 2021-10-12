355145b9674e1551c92629456fc211ee2eef877c
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-10-12 03:03:12 +0300

    feat: return_nth_value_in_fibonacci_series_6

--- math_series/series.py
+++ math_series/series.py
@@ -4,7 +4,9 @@
 # pylint: disable=missing-function-docstring
 
 
-def fibonacci(n: int):
+def fibonacci(n: int) -> int:
+    if n <= 0:
+        return 0
     fibonacci_sequence = [0, 1]
     for i in range(n):
         nth_value = fibonacci_sequence[i] + fibonacci_sequence[i+1]
